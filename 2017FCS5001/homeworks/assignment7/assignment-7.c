59ed28ed147b9796004149399238e3da53cda1b7
Wenxuan <gwx2013@mail.ustc.edu.cn>
2017-10-10 12:24:02 -0700

    assignment 7

--- 2017FCS5001/homeworks/assignment7/assignment-7.c
+++ 2017FCS5001/homeworks/assignment7/assignment-7.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+	double x;
+	double y;
+} Point;
 
+Point *createPoint(double x, double y);
+
+
+
+
+Point *createPoint(double x, double y) {
+	Point *p = malloc(sizeof(Point));
+	p->x = x;
+	p->y = y;
+	return p;
+}
+x	
